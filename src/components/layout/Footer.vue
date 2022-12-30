51b771c32e261d2ac3adc36cf5ae7f309a4c33da
Haytham Salama <haythamasalama@gmail.com>
2022-12-30 23:53:44 +0200

    feat(components): add layout components

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+  import IconGithub from '../icons/IconGithub.vue';
+  import IconTwitter from '../icons/IconTwitter.vue';
+</script>
+
+<template>
+  <footer
+    class="text-gray-400 text-base font-medium flex flex-col items-center container mx-auto p-4 capitalize"
+  >
+    <!-- source code button -->
+    <a
+      class="flex text-white my-4 border-4 transition-primary p-3 rounded hover:bg-primary-gradient hover:ring-2 hover:ring-primary-gradient"
+      href="https://github.com/HaythamaSalama/cipherTool"
+      target="_blank"
+    >
+      <IconGithub class="mx-2" />
+      source code
+    </a>
+
+    <!-- developed by  -->
+    <div class="flex my-2">
+      <p class="mr-2">
+        made with ❤️ by
+        <a
+          class="font-bold hover:text-primary transition-primary"
+          href="http://haythamasalama.me/"
+          target="_blank"
+        >
+          haytham salama
+        </a>
+      </p>
+
+      <div class="flex">
+        <a
+          href="https://github.com/HaythamaSalama"
+          target="_blank"
+        >
+          <IconGithub class="mx-2" />
+        </a>
+        <a
+          href="https://twitter.com/HaythamaSalama"
+          target="_blank"
+        >
+          <IconTwitter class="mx-2" />
+        </a>
+      </div>
+    </div>
+
+    <!-- copyright -->
+    <div class="my-2">© 2022 cipher tool. all rights reserved.</div>
+  </footer>
+</template>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="mt-4">
+    <div class="flex flex-col items-center justify-center container mx-auto p-4">
+      <h1 class="font-extrabold text-3xl drop-shadow text-primary-gradient capitalize">
+        cipher tool
+      </h1>
+      <div class="mt-2">
+        <p class="text-gray-400 text-center pb-3 capitalize">
+          Encrypt and decrypt messages using Hill Cipher and DES in CBC mod.
+        </p>
+      </div>
+    </div>
+  </header>
+</template>
