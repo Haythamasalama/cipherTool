8c044edb750ee17da7cdd992b221196b7299c7f8
Haytham A. Salama <haythamasalama@gmail.com>
2023-07-03 17:56:45 +0300

    chore: remove base option

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: 'https://github.com/Haythamasalama/cipherTool/',
   plugins: [vue()],
   resolve: {
     alias: {
